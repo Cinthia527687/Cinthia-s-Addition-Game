ed0a436185cc762cbac59083425b79554b6a18e5
Cinthia Alvarez <alvarezc@student.swosu.edu>
2015-10-09 10:40:15 -0500

    added dev1412 branch

--- src/AdditionGame.java
+++ src/AdditionGame.java
@@ -2,6 +2,22 @@
 public class AdditionGame {
 	public static void main(String[] args) {
 		//Cinthia's Addition Game
+		//Development 1412 branch
+		
+		//Outline for addition problem.
+		
+		//Round 1
+		// Generate 2 Random numbers
+		// Ask the user to add these two numbers together
+		// Read in their response
+		// Check if the answer was correct
+		//  	IF correct
+		//  		Give them points
+		//  		Make next question harder
+		//  		Tell them they are correct
+		//  	IF wrong
+		//  		Tell them they are wrong, why, and correct the answer
+		//  		Make next question easier
 
 	}
 
