79503c9226fa3b10c74ee62cfcca79080a1824e7
Cinthia Alvarez <alvarezc@student.swosu.edu>
2015-10-23 03:58:16 -0500

    Finished round 1 IF Correct

--- src/AdditionGame.java
+++ src/AdditionGame.java
@@ -14,21 +14,30 @@ public class AdditionGame {
 		// Generate 2 Random numbers
 			int number1 = (int)(Math.random() * hardness);
 			int number2 = (int)(Math.random() * hardness);
-			int correctAnswer = number1 = number 2;
+			int correctAnswer = number1 = number2;
 			
 		// Ask the user to add these two numbers together
 			System.out.println("What number is" + number1 + "+" + number2 + "?");
 			
 		// Read in their response
-			Scanner input - new Scanner(System.in);
+			Scanner input = new Scanner(System.in);
 			int studentAnswer = input.nextInt();
 			
 		// Check if the answer was correct
 			if(studentAnswer == correctAnswer){
-				//  IF correct
-				//  	Give them points
-				//  	Make next question harder
-				//  	Tell them they are correct
+				
+				// IF correct
+				
+					// Tell them it was correct
+						System.out.println("Your anser was correct.");
+						
+					// Give them points
+					score += hardness;
+					System.out.println("Your score is now: " + score);
+		
+					// Make next question harder
+						hardness *= 10;
+					
 				// 	IF wrong
 				// 		Tell them they are wrong, why, and correct the answer
 				// 		Make next question easier
