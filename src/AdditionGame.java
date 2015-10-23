7f46e0d50f5d7b0ced284208c3bb7356b73ab566
Cinthia Alvarez <alvarezc@student.swosu.edu>
2015-10-23 04:34:22 -0500

    Finished fourth round

--- src/AdditionGame.java
+++ src/AdditionGame.java
@@ -177,19 +177,63 @@ public class AdditionGame {
 					
 			System.out.println("End of third round.");
 					
-		//Round 4
+	//Round 4
+			
 		// Generate 2 Random numbers
+			int number1 = (int)(Math.random() * hardness);
+			int number2 = (int)(Math.random() * hardness);
+			int correctAnswer = number1 = number2;
+					
 		// Ask the user to add these two numbers together
+			System.out.println("What number is" + number1 + "+" + number2 + "?");
+					
 		// Read in their response
+			Scanner input = new Scanner(System.in);
+			int studentAnswer = input.nextInt();
+					
 		// Check if the answer was correct
-		//  	IF correct
-		//  		Give them points
-		//  		Make next question harder
-		//  		Tell them they are correct
-		//  	IF wrong
-		//  		Tell them they are wrong, why, and correct the answer
-		//  		Make next question easier
-		
+			if(studentAnswer == correctAnswer){
+						
+				// IF correct
+						
+					// Tell them it was correct
+						System.out.println("Your answer was correct.");
+								
+					// Give them points
+						score += hardness;
+						System.out.println("Your score is now: " + score);
+				
+					// Make next question harder
+						hardness *= 10;
+							
+				// IF incorrect
+								
+					// Tell them they are incorrect
+						System.out.println("Your answer was incorrect.");
+								
+					// Tell them the correct answer
+						System.out.println("The correct answer was: " + correctAnswer);
+								
+					// Don't give points
+						score += 0;
+								
+					// Make next question easier
+						if(hardness>10){
+									
+							hardness /= 10;
+							System.out.println("Your difficulty is now: " + hardness);
+									
+						}else{
+										
+							System.out.println("Your difficulty is at the lowest level");
+							System.out.println("Your difficulty is now: " + hardness);
+						}
+								
+			}
+					
+			System.out.println("End of fourth round.");
+			System.out.println("Your final score is: " + score);
+					
 
 	}
 
